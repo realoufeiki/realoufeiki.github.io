4a130b53185ddce25f6c559c51426a841fb4e1ac
realoufeiki <91794335+realoufeiki@users.noreply.github.com>
2023-08-22 08:30:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function analisa() {
+    // Gera um número aleatório entre 0 e 1
+    var randomValue = Math.random();
 
-    alert('Essa notícia é real.');
+    if (randomValue < 0.5) {
+        alert('Essa notícia é real.');
+    } else {
+        alert('Essa notícia é fake.');
+    }
 }
- let botaoEl = document.querySelector('#teste')
- botaoEl.addEventListener('click', analisa)
+
+let botaoEl = document.querySelector('#teste');
+botaoEl.addEventListener('click', analisa);
